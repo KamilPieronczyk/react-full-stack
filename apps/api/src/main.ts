5017e263e1513c425a4fec4f8701bd184314305e
Kamil <kamil.pieronczyk@euvic.pl>
2023-02-24 22:42:07 +0100

    prisma

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,15 +3,29 @@
  * This is only a minimal backend to get started.
  */
 
+import { PrismaClient } from '@react-full-stack/database';
 import express from 'express';
 import * as path from 'path';
 
 const app = express();
 
+const prisma = new PrismaClient();
+await prisma.$connect();
+
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
+});
+
+app.get('/test', async (req, res) => {
+  await prisma.user.create({
+    data: {
+      email: 'test',
+    },
+  });
+
+  res.send({ message: 'Welcome to test!' });
 });
 
 const port = process.env.PORT || 3000;
--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -4,5 +4,8 @@ const { composePlugins, withNx } = require('@nrwl/webpack');
 module.exports = composePlugins(withNx(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
+  config.experiments = {
+    topLevelAwait: true,
+  };
   return config;
 });
