511bd41c35b359d5d64f1d4cbba60fd96cec3d41
Kamil <kamilpier08@wp.pl>
2023-03-24 19:55:54 +0100

    auth0 init

--- apps/api/src/core/auth/auth.module.ts
+++ apps/api/src/core/auth/auth.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './services/jst-strategy.service';
 
-@Module({})
+@Module({
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
+  providers: [JwtStrategy],
+  exports: [PassportModule],
+})
 export class AuthModule {}
--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Auth0Provider } from '@auth0/auth0-react';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -9,12 +10,22 @@ import './styles.css';
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Welcome to frontend!</title>
-      </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
+      <Auth0Provider
+        domain="dev-react-full-stack.eu.auth0.com"
+        clientId="cFTYdmARn7tuaHtaDlBaD1tAfdXEbxOK"
+        authorizationParams={{
+          redirect_uri: 'http://localhost:4200/auth/auth',
+          audience: 'https://dev-react-full-stack.eu.auth0.com/api/v2/',
+          scope: 'read:current_user update:current_user_metadata',
+        }}
+      >
+        <Head>
+          <title>Welcome to frontend!</title>
+        </Head>
+        <main className="app">
+          <Component {...pageProps} />
+        </main>
+      </Auth0Provider>
     </>
   );
 }
